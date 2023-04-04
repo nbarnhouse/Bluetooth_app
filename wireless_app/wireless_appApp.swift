476011c46dfcbdaa44b0119c6da4b67a42d712c0
Nicole <lakehouse624@icloud.com>
2023-04-03 21:44:09 -0500

    Initial Commit

---
+++ wireless_app/wireless_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wireless_appApp.swift
+//  wireless_app
+//
+//  Created by Nicole Barnhouse on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct wireless_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
